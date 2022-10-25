ad4cbf66b884bc05be99dae9ae5e64058144c1c3
William Bushie <willbushie@gmail.com>
2022-10-25 09:32:12 -0500

    general basic design complete - moving to clean & add functionality

---
+++ app/src/main/java/com/example/photoshare/ActivityHomeJoined.java
@@ -0,0 +1,65 @@
+package com.example.photoshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityHomeJoined extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_joined);
+    }
+
+    public void shareGroupSession(View v) {
+        /*
+        Share the group session (via a link or viewing the QR Code).
+
+         */
+
+    }
+
+    public void leaveShareGroup(View v) {
+        /*
+        Leave the share group.
+
+         */
+
+    }
+
+    public void viewCurrentTimeline(View v) {
+        /*
+        View the current timeline, but allow moving to all timeline view.
+
+         */
+
+    }
+
+    public void openSettingsPage(View v) {
+        /*
+        Open the settings page.
+
+         */
+
+    }
+
+    public void editShareGroup(View v) {
+        /*
+        Allow any user to suggest changes, prompt everyone on accepting those changes.
+
+         */
+
+    }
+
+    public void pauseShare(View v) {
+        /*
+        Pause sharing with others in the group for the given time.
+
+         */
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/photoshare/ActivitySettingsProfile.java
@@ -0,0 +1,44 @@
+package com.example.photoshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ActivitySettingsProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_profile);
+    }
+
+    public void updateProfileEmail(View v) {
+        /*
+        Update a user's email.
+
+         */
+
+    }
+
+    public void updateProfilePassword(View v) {
+        /*
+        Update a user's password.
+
+         */
+
+    }
+
+    public void appLogout(View v) {
+        /*
+        Logout of the application
+
+         */
+
+    }
+
+}
