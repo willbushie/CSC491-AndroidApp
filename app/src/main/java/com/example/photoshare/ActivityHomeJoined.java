e07f53e53edb0bf0b4b8cc4b92c08c629794efd7
William Bushie <willbushie@gmail.com>
2022-11-12 13:50:04 -0600

    leave confirmation, hide settings back button

--- app/src/main/java/com/example/photoshare/ActivityHomeJoined.java
+++ app/src/main/java/com/example/photoshare/ActivityHomeJoined.java
@@ -1,5 +1,7 @@
 package com.example.photoshare;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -24,16 +26,6 @@ public class ActivityHomeJoined extends AppCompatActivity {
         Initialize all buttons so they are all prepared once the activity is loaded.
         This is only for buttons that require OnClickListeners.
          */
-
-        // Initialze Leave Button
-        // MODIFY THIS TO BRING UP A MENU FIRST TO CONFIRM, THEN CONDUCT THE ACTION
-        Button button_leave = (Button)findViewById(R.id.button_leave);
-        button_leave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ActivityHomeJoined.this, ActivityHomeUnjoined.class));
-            }
-        });
 
         // Initialize Settings Button
         Button open_settings_button = (Button)findViewById(R.id.button_settings);
@@ -63,6 +55,22 @@ public class ActivityHomeJoined extends AppCompatActivity {
         Button functionality (this method) will still work onClick.
          */
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityHomeJoined.this);
+        builder.setMessage("Are you sure you want to leave the group?");
+        builder.setTitle("Are you sure?");
+
+        // if confirmed
+        builder.setPositiveButton("Yes, Leave", (DialogInterface.OnClickListener) (dialog, which) -> {
+            finish();
+        });
+        // if canceled
+        builder.setNegativeButton("Cancel", (DialogInterface.OnClickListener) (dialog, which) -> {
+            dialog.cancel();
+        });
+
+        // Create  & show Alert dialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     public void editShareGroup(View v) {
