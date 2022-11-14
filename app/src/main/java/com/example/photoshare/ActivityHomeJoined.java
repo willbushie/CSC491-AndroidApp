97ff580f10f202217baf66d60486e12d03fd547d
William Bushie <willbushie@gmail.com>
2022-11-13 19:17:02 -0600

    qr generation begining alert dialog - does not work

--- app/src/main/java/com/example/photoshare/ActivityHomeJoined.java
+++ app/src/main/java/com/example/photoshare/ActivityHomeJoined.java
@@ -5,14 +5,24 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 import android.content.ClipboardManager;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.WriterException;
+
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
@@ -65,8 +75,20 @@ public class ActivityHomeJoined extends AppCompatActivity {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         if (item.getItemId() == R.id.menu_item_showQRCode) {
-                            Toast.makeText(ActivityHomeJoined.this, "Showing QR Code", Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(ActivityHomeJoined.this, "Showing QR Code", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(ActivityHomeJoined.this, ActivityShareableQRCode.class));
+                            /*/ This alert dialog will show the qr code (dynamically changing) - DOES NOT CURRENTLY WORK
+                            AlertDialog.Builder builder = new AlertDialog.Builder(ActivityHomeJoined.this);
+                            LayoutInflater factory = LayoutInflater.from(ActivityHomeJoined.this);
+                            final View view = factory.inflate(R.layout.activity_shareable_qrcode, null);
+
+                            builder.setView(view);
+                            builder.setTitle("Scan QR Code to Join");
+                            builder.setNeutralButton("Close", (DialogInterface.OnClickListener) (dialog, which) -> {
+                                dialog.cancel();
+                            });
+                            AlertDialog alert = builder.create();
+                            alert.show();*/
                         }
                         else if (item.getItemId() == R.id.menu_item_copyLink) {
                             Toast.makeText(ActivityHomeJoined.this, "Copied Link to Clipboard", Toast.LENGTH_SHORT).show();
@@ -146,6 +168,49 @@ public class ActivityHomeJoined extends AppCompatActivity {
             button_pause.setText(R.string.button_text_pause);
             button_pause.setBackgroundColor(getColor(R.color.easy_green));
             button_pause.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.ic_baseline_pause_24, 0, 0);
+        }
+    }
+
+    public void generateQRCode() {
+        /*
+        This method will set the QR code in this view to the share URL.
+        This method does not work inside of this java file - for now a new activity is used for scanning qr codes
+         */
+
+        // variable initialization
+        ImageView qrCodeIV = findViewById(R.id.idIVQrcode);
+        Bitmap bitmap;
+        QRGEncoder qrgEncoder;
+
+        // below line is for getting the windowmanager service.
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        // initializing a variable for default display.
+        Display display = manager.getDefaultDisplay();
+
+        // creating a variable for point which is to be displayed in QR Code.
+        Point point = new Point();
+        display.getSize(point);
+
+        // getting width and height of a point
+        int width = point.x;
+        int height = point.y;
+
+        // generating dimension from width and height.
+        int dimen = width < height ? width : height;
+        dimen = dimen * 3 / 4;
+
+        // setting this dimensions inside our qr code encoder to generate our qr code.
+        //qrgEncoder = new QRGEncoder(dataEdt.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+        qrgEncoder = new QRGEncoder("https://photoshare.com/api/group/join/788", null, QRGContents.Type.TEXT, dimen);
+        try {
+            // getting our qrcode in the form of bitmap.
+            bitmap = qrgEncoder.encodeAsBitmap();
+            // the bitmap is set inside our image view using .setimagebitmap method.
+            qrCodeIV.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            // this method is called for exception handling.
+            Log.e("Tag", e.toString());
         }
     }
 
